3c8414af1db439f7facc39d333325f1e1ffd2470
ali <7731.aa@gmail.com>
2023-10-13 17:31:13 +0330

    Fetching games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import useData, { FetchResponse } from "./useData";
 import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -17,18 +19,29 @@ export interface Game {
   rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+  queryKey: ["games", gameQuery],
+  queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {
+    params: {
+      genres: gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder,
+      search: gameQuery.searchText
+    }
+  }).then(res => res.data),
+  staleTime: 1 * 60 * 60 * 1000 // 1h
+})
+  // useData<Game>(
+  //   "/games",
+  //   {
+  //     params: {
+  //       genres: gameQuery.genre?.id,
+  //       platforms: gameQuery.platform?.id,
+  //       ordering: gameQuery.sortOrder,
+  //       search: gameQuery.searchText
+  //     },
+  //   },
+  //   [gameQuery]
+  // );
 
 export default useGames;

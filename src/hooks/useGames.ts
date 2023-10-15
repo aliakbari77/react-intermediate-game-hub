a282f777e5c8942ad4a1824b0b6c9935f8b0bc24
Ali Akbari <7731.aa@gmail.com>
2023-10-15 16:50:44 +0330

    Refactor- simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,31 +4,34 @@ import { FetchResponse } from "../services/api-client";
 import APIClient from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
-const apiClient = new APIClient<Game>("/games")
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+	id: number;
+	name: string;
+	background_image: string;
+	parent_platforms: { platform: Platform }[];
+	metacritic: number;
+	rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
-  queryKey: ["games", gameQuery],
-  queryFn: ({pageParam = 1}) => apiClient.getAll({ params: {
-    genres: gameQuery.genre?.id,
-    parent_platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText,
-    page: pageParam
-  }
-}),
-  getNextPageParam: (lastPage, allPages) => {
-    return lastPage.next ? allPages.length + 1 : undefined
-  },
-  staleTime: 24 * 60 * 60 * 1000 //24h
-})
+const useGames = (gameQuery: GameQuery) =>
+	useInfiniteQuery<FetchResponse<Game>, Error>({
+		queryKey: ["games", gameQuery],
+		queryFn: ({ pageParam = 1 }) =>
+			apiClient.getAll({
+				params: {
+					genres: gameQuery.genreId,
+					parent_platforms: gameQuery.platformId,
+					ordering: gameQuery.sortOrder,
+					search: gameQuery.searchText,
+					page: pageParam,
+				},
+			}),
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.next ? allPages.length + 1 : undefined;
+		},
+		staleTime: 24 * 60 * 60 * 1000, //24h
+	});
 
 export default useGames;
